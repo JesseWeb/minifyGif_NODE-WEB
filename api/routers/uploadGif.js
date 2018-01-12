34fc2508d761433981606bdb29beff141450cd00
wangxuhuai <395731482@qq.com>
2018-01-12 16:32:17 +0800

    node端使用async函数处理批量上传图片处理后的顺序混乱问题

--- api/routers/uploadGif.js
+++ api/routers/uploadGif.js
@@ -5,7 +5,7 @@ let fs = require('fs')
 let path = require('path')
 let port = process.env.PORT
 const formidable = require('formidable')
-let portStr = process.env.PORT!=80?':'+process.env.PORT:''
+let portStr = process.env.PORT != 80 ? ':' + process.env.PORT : ''
 router.post('/', (req, res, next) => {
     res.header({
         "content-type": "application/json",
@@ -22,54 +22,33 @@ router.post('/', (req, res, next) => {
                 data: 'error'
             })
         } else {
+
             let gap = fields.gap
             let quality = fields.quality
             let size = fields.size
-            let index = fields.index
             miniGif({
                 gap,
                 quality,
                 files,
-                size,
-                index
-            }).then(({paths, code}) => {
+                size
+            }).then((result) => {
                 let arr = []
-                paths.forEach(obj => {
+                result.forEach(obj => {
                     arr.push({
-                        url:'http://'+req.hostname+portStr+'/product/'+obj.url,
-                        size:fs.statSync('./product/'+obj.url).size,
-                        index:obj.index,
-                        name:obj.name,
-                        originSize:obj.originSize
+                        url: 'http://' + req.hostname + portStr + '/product/' + obj.url,
+                        size: fs.statSync('./product/' + obj.url).size,
+                        name: obj.name,
+                        originSize: obj.originSize
                     })
                 });
-
-                if (code == 0) {
-                    res.status(200)
-                    res.json({
-                        code: 0,
-                        message: "success",
-                        data: {
-                            imageUrls:arr
-                        }
-                    })
-                }
-            }, (err, code) => {
-                if (code == 1) {
-                    res.status(415)
-                    res.json({
-                        code: 0002,
-                        message: "图片生成失败",
-                        data: 'error'
-                    })
-                } else if (code == -1) {
-                    res.status(415)
-                    res.json({
-                        code: 0003,
-                        message: "图片解析失败",
-                        data: 'error'
-                    })
-                }
+                res.status(200)
+                res.json({
+                    code: 0,
+                    message: "success",
+                    data: {
+                        imageUrls: arr
+                    }
+                })
             })
         }
     });
